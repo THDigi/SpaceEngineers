15aeb6f34a5e97b878eed8b12e15b003ab6d38af
Digi <hunter_digital_sl@yahoo.com>
2015-06-20 19:34:56 +0300

    WIP

--- Sources/Sandbox.Common/ModAPI/Ingame/IMyCockpit.cs
+++ Sources/Sandbox.Common/ModAPI/Ingame/IMyCockpit.cs
@@ -7,5 +7,14 @@ namespace Sandbox.ModAPI.Ingame
 {
     public interface IMyCockpit : IMyShipController
     {
+        /// <summary>
+        /// Cockpit's oxygen level, 0 to 1 range.
+        /// </summary>
+        float OxygenLevel { get; }
+
+        /// <summary>
+        /// Cockpit's max oxygen capacity, in O2 units.
+        /// </summary>
+        float OxygenCapacity { get; }
     }
 }
---
+++ Sources/Sandbox.Game/ModAPI/MyCockpit_ModAPI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sandbox.ModAPI.Ingame;
+
+namespace Sandbox.Game.Entities
+{
+    public partial class MyCockpit : Sandbox.ModAPI.IMyCockpit
+    {
+        float IMyCockpit.OxygenLevel
+        {
+            get { return OxygenLevel; }
+        }
+
+        float IMyCockpit.OxygenCapacity
+        {
+            get { return BlockDefinition.OxygenCapacity; }
+        }
+    }
+}
